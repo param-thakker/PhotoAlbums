bdfb72cfbd6fed245de7c928230fdc97f7864898
jnl27 <jnl76@rutgers.edu>
2021-04-10 19:27:28 -0400

    login screen now displays

--- src/photos/Photos.java
+++ src/photos/Photos.java
@@ -1,17 +1,32 @@
 package photos;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import view.LoginController;
 
 public class Photos extends Application {
 
 	public static void main(String[] args) {
-		
-
+		launch(args);
 	}
 
-	public void start(Stage arg0) throws Exception {
-		
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();   
+		loader.setLocation(
+				getClass().getResource("/view/Login.fxml"));
+		AnchorPane root = (AnchorPane)loader.load();
+
+		LoginController loginControl = 
+				loader.getController();
+		loginControl.start(primaryStage);
+
+		Scene scene = new Scene(root, 600, 400);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Photo Library");
+		primaryStage.show(); 
 		
 	}
 
---
+++ src/view/LoginController.java
@@ -0,0 +1,41 @@
+package view;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+
+public class LoginController {
+	
+	@FXML
+	TextField username;
+	
+	@FXML
+	Button login;
+	
+	//list of users & admin
+	HashSet<String> users = new HashSet<String>();
+	
+	public void start(Stage mainStage) throws IOException{
+		
+	}
+}
