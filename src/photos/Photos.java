16e9a547e8a62b99b809f229732a6f672be6a7be
paramthakker <37357102+paramthakker@users.noreply.github.com>
2021-04-10 02:07:33 -0400

    Boilerplate framework

---
+++ src/photos/Photos.java
@@ -0,0 +1,18 @@
+package photos;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Photos extends Application {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	public void start(Stage arg0) throws Exception {
+		
+		
+	}
+
+}
