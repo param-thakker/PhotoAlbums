f5f8c9145e83ac701bf5947f03a9491c78b42c60
jnl27 <jnl76@rutgers.edu>
2021-04-11 01:59:48 -0400

    added fxml tags

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -24,7 +24,82 @@ import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
 public class UserController {
+	@FXML
+	Button logout;
+	@FXML
+	Button newAlbum;
+	@FXML
+	Button renameAlbum;
+	@FXML
+	Button delAlbum;
+	@FXML
+	Button openAlbum;
+	@FXML
+	Button search;
+	@FXML
+	Button mcConfirm;
+	@FXML
+	Button mcCancel;
+	@FXML
+	Button backToAlbum;
+	@FXML
+	Button addPhoto;
+	@FXML
+	Button delPhoto;
+	@FXML
+	Button movePhoto;
+	@FXML
+	Button copyPhoto;
+	@FXML
+	Button addTag;
+	@FXML
+	Button delTag;
+	@FXML
+	Button prevPhoto;
+	@FXML
+	Button nextPhoto;
+	@FXML
+	Button sLogout;
+	@FXML
+	Button sCreateAlbum;
+	@FXML
+	Button sBacktoAlbums;
+	@FXML
+	TextField caption;
+	@FXML
+	TextField dateCaptured;
+	@FXML
+	TextField Tags;
+	@FXML
+	ListView albumList;
+	@FXML
+	ListView mcAlbumList;
+	@FXML
+	ListView photoList;
+	@FXML
+	ListView searchResults;
+	
+	
 	public void start(Stage mainStage) throws IOException{
 		
 	}
+	
+	public void search(ActionEvent e) throws IOException{
+		
+	}
+    public void logout(ActionEvent e) throws IOException{
+		
+	}
+    public void newAlbum(ActionEvent e) throws IOException{
+		
+	}
+    public void renameAlbum(ActionEvent e) throws IOException{
+		
+	}
+    public void delAlbum(ActionEvent e) throws IOException{
+    	
+    }
+    public void openAlbum(ActionEvent e) throws IOException{
+    	
+    }
 }
