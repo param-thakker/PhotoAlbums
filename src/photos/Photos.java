d5c461429031d65d7bb43e830f9d85d7f1fd5fe5
jnl27 <jnl76@rutgers.edu>
2021-04-11 01:35:34 -0400

    fxml commits now working properly

--- src/photos/Photos.java
+++ src/photos/Photos.java
@@ -1,6 +1,6 @@
 package photos;
 
-import controller.LoginController;
+import controller.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,13 +15,17 @@ public class Photos extends Application {
 
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader();   
-		loader.setLocation(getClass().getResource("/view/Login.fxml"));
+		loader.setLocation(getClass().getResource("/view/SearchDisplay.fxml"));
 		AnchorPane root = (AnchorPane)loader.load();
+		
+		//AdminController admin = loader.getController();
+		//admin.start(primaryStage);
+		//LoginController loginControl = loader.getController();
+		//loginControl.start(primaryStage);
+		UserController user = loader.getController();
+		user.start(primaryStage);
 
-		LoginController loginControl = loader.getController();
-		loginControl.start(primaryStage);
-
-		Scene scene = new Scene(root, 600, 400);
+		Scene scene = new Scene(root, 600, 400); //resize as needed
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("User Login");
 		primaryStage.show(); 
